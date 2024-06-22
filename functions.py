ea7270a7439597da6aa834d965d8480ed99e7986
peterjosep <mwangijosephpeter@gmail.com>
2024-06-22 13:42:05 +0300

    send sms function

--- functions.py
+++ functions.py
@@ -54,3 +54,22 @@ def passwordValidity(password):
         return True
 
 # passwordValidity(input("Enter your password: "))
+
+# sending an sms
+import africastalking
+africastalking.initialize(
+username="joe2022",
+api_key="aab3047eb9ccfb3973f928d4ebdead9e60beb936b4d2838f7725c9cc165f0c8a"
+#justpaste.it/1nua8
+)
+sms = africastalking.SMS
+def send_sms(phone, message):
+    recipients = [phone]
+    sender = "AFRICASTALKING"
+    try:
+        response = sms.send(message, recipients)
+        print(response)
+    except Exception as error:
+        print("Error is",  error )
+# send_sms("+254716988147", "This is my message")
+
