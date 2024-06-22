b37cbfd3541aee8948e7e9080552d9c0c0315236
peterjosep <mwangijosephpeter@gmail.com>
2024-06-22 13:40:16 +0300

    function to check phone validity

--- functions.py
+++ functions.py
@@ -16,34 +16,23 @@ def gen_random():
     return str(res)
 
 
+# gen_random()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# check phone validity
+import re
+# its a module which provides support for working with regular expressions
+def  check_phone(phone):
+    # the function takes one argument phone
+    regex = "^\+254\d{9}"
+    # ^ it asserts the start of the string
+    # \+254 ir matches the literal string +254
+    # \d{9} matches exactly 9 digits
+    if not re.match(regex, phone) or len(phone) != 13:
+        print("Phone is not valid")
+        return False
+    else:
+        print("Phone is valid, OK")
+        return True
+# phone_number = input("Enter your number")
+# check_phone(phone_number)
 
